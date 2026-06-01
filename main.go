2a4b629eb331e34c4997a79285cfb1781769292e
NatsuiroGinga <duyueyeweiliang@gmail.com>
2026-06-01 15:27:53 +0800

    更新 DNS 处理程序，添加 A 记录响应并引入依赖项

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+
+	"github.com/miekg/dns"
+)
+
+var cndConfig = map[string]string{
+	"web.cdn.test.c.ginga.": "127.0.0.1",
+}
+
+func handler(writer dns.ResponseWriter, req *dns.Msg) {
+	var resp dns.Msg
+	resp.SetReply(req)
+
+	for _, question := range req.Question {
+		ip := cndConfig[question.Name]
+		if len(ip) == 0 {
+			return
+		}
+		recordA := dns.A{
+			Hdr: dns.RR_Header{
+				Name:   question.Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    100,
+			},
+			A: net.ParseIP(ip).To4(), // 全部解析为127.0.0.1
+		}
+		resp.Answer = append(resp.Answer, &recordA) // 写入应答
+	}
+	err := writer.WriteMsg(&resp) // 回写信息
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	fmt.Println("hello")
+	dns.HandleFunc(".", handler)                   // 绑定函数
+	err := dns.ListenAndServe(":1053", "udp", nil) // 启动
+	if err != nil {
+		log.Println(err)
+	}
 }
